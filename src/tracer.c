0cfd56a7b3349dc79cf02d2b5d4eb224ff991f4a
Swistusmen <michal76aw@gmail.com>
2024-04-12 14:19:23 +0200

    fix

--- src/tracer.c
+++ src/tracer.c
@@ -1359,7 +1359,7 @@ static int __tracer_transition_tracing(
 #endif                
 #ifdef HAVE_THAW_BDEV_INT
                 ret = thaw_bdev(bdev, sb);
-#elif defined HAVE_BDEV_THAT
+#elif defined HAVE_BDEV_THAW
                 ret=bdev_thaw(bdev);
 #else
                 ret = thaw_bdev(bdev);
